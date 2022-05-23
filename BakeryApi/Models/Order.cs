196aafcba2d2372fbba6b592c420a9e8b12a0b57
Eugenio <eugeniolimaeugenio5000@outlook.com>
2022-05-22 21:34:19 -0300

    #1 Create client, order, orderItem and product templates

---
+++ BakeryApi/Models/Order.cs
@@ -0,0 +1,12 @@
+﻿namespace BakeryApi.Models
+{
+    public class Order
+    {
+        public int Nr_Order { get; set; }
+        public DateTime DT_Order { get; set; }
+        public string? Type { get; set; }
+        public Client? Client { get; set; }
+        public List<OrderItem>? Items { get; set; }
+
+    }
+}
---
+++ BakeryApi/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace BakeryApi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Description { get; set; }
+        public int Stock { get; set; }
+        public decimal Value { get; set; }
+    }
+}
