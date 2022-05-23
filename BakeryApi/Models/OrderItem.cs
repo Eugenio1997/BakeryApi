7d1d004473759aebf351241ee6038846cbb613e6
Eugenio <eugeniolimaeugenio5000@outlook.com>
2022-05-23 17:36:39 -0300

    #5 Fix models OrderItem and Product
    For example, It was inserted this annotation on code as follow below:
     [Column(TypeName = "decimal(18,4)")]

--- BakeryApi/Models/OrderItem.cs
+++ BakeryApi/Models/OrderItem.cs
@@ -1,9 +1,12 @@
-﻿namespace BakeryApi.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BakeryApi.Models
 {
     public class OrderItem
     {
         public int Id { get; set; }
         public int Quantity { get; set; }
+        [Column(TypeName = "decimal(18,4)")]
         public decimal Unitary_Value { get; set; }
         public Product? Product { get; set; }
     }
